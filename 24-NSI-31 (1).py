d11cfd326a1b0746305bec1f4332e320a5176d79
Math2994 <112695513+Math2994@users.noreply.github.com>
2024-06-04 13:17:51 +0000

    a

---
+++ 24-NSI-31 (1).py
@@ -0,0 +1,40 @@
+# Ex1
+def multiplication(n1, n2):
+    if n1 <= n2:
+        nbre_addition = n2
+        n2 = n1
+    else:
+        nbre_addition = n1
+        n1 = n2
+    if nbre_addition > 0:
+        while nbre_addition > 1:
+            n1 = n1 + n2
+            nbre_addition -= 1
+    elif nbre_addition < 0:
+        while nbre_addition < -1:
+            n1 = n1 + n2
+            nbre_addition += 1
+    return n1
+
+
+# Ex2
+def dichotomie(tab, x):
+    """
+    tab : tableau d'entiers trié dans l'ordre croissant
+    x : nombre entier
+    La fonction renvoie True si tab contient x et False sinon
+    """
+    debut = 0
+    fin = len(tab) - 1
+    while debut <= fin:
+        m = (debut+fin)//2
+        if x == tab[m]:
+            return True
+        if x > tab[m]:
+            debut = m + 1
+        else:
+            fin = m - 1
+    return False
+
+
+
